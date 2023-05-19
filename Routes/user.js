031dcf57bd99d9b2fe573fd22094257a4fc3a77c
cceer <sisir.sital@gmail.com>
2023-05-19 17:34:55 +0545

    feat: get user added

--- Routes/user.js
+++ Routes/user.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const login = require('../controllers/auth/loginUser');
 const signup = require('../controllers/auth/registerUser');
+const authProtect = require('../middleware/authProtect');
 
 const router = express.Router();
 
 // router.post('/register', signup);
+router.get('/', authProtect, (req, res) => {
+    return res.status(200).json({ user: { email: req.user.email } });
+});
 
 router.post('/login', login);
 
